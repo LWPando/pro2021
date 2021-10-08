c286b772841f32903566149b41565b8cea695da0
Masa <inf.1489@scng.si>
2021-10-08 12:36:26 +0200

    palindrom

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class JePalindrome
+{
+    public static bool IsPalindrome(string text)
+    {
+        if (text.Length <= 1)
+            return true;
+        else
+        {
+            if (text[0] != text[text.Length - 1])
+                return false;
+            else
+                return IsPalindrome(text.Substring(1, text.Length - 2));
+        }
+    }
+    public static void Main()
+    {
+        Console.Write("\n\n Preveri, ali je beseda palindrom ali ne :\n");
+        Console.Write("---------------------------------------------------------------\n");
+        string str1;
+        bool tf;
+
+        Console.Write(" Vpisi besedo : ");
+        str1 = Console.ReadLine();
+        tf = IsPalindrome(str1);
+        if (tf == true)
+        {
+            Console.WriteLine(" Beseda je palindrom.\n");
+        }
+        else
+        {
+            Console.WriteLine(" Beseda ni palindrom.\n");
+        }
+    }
+}
